985d4f84e2e45763b80e85a50c7683eda2417dbd
jessechen0319 <xiang19890319@gmail.com>
2015-08-24 22:57:38 +0800

    对话框初步

--- pray/public/javascripts/index/index.js
+++ pray/public/javascripts/index/index.js
@@ -1,5 +1,5 @@
 var indexapp = angular.module("indexapp", ['ui.bootstrap']);
-indexapp.controller("IndexController", function($scope, $http) {
+indexapp.controller("IndexController", function($scope, $http, $modal) {
 	$scope.isLogined = false;
 	$scope.submitClass = 'btn';
 	$scope.prayData = [];
@@ -10,12 +10,46 @@ indexapp.controller("IndexController", function($scope, $http) {
 	$scope.insertRecord = function(){
 
 		//prepare data content
-		$scope.submitClass = 'disabled btn';
+
+		var modalInstance = $modal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: 'confirmAdd.html',
+	      controller: 'ConfirmAddController',
+	      resolve: {
+	        submitObject: function () {
+	          return {'name': $scope.name,
+					'date': $scope.date,
+					'content': $scope.content};
+	        }
+	      }
+    	});
+
+    	modalInstance.result.then(function (submitObject) {
+		     	if(submitObject.name && submitObject.content && submitObject.date){
+					//main request			
+					$http(
+						{method: 'POST',
+						params: submitObject,
+						url: "/pray/insertRecord"
+					}).success(function(data){
+						$scope.submitClass = 'btn';
+						$scope.getPrayRecords();
+					}).error(function(){
+						$scope.submitClass = 'btn';
+					});
+				} else {
+					console.log($modal);
+					$scope.submitClass = 'btn';
+				}
+		    }, function () {
+		     	$log.info('Modal dismissed at: ' + new Date());
+		    });
+
+		/*$scope.submitClass = 'disabled btn';
 		if($scope.name && $scope.content && $scope.date){
 			var dataContent = {'name': $scope.name,
 					'date': $scope.date,
 					'content': $scope.content};
-
 			//main request			
 			$http(
 				{method: 'POST',
@@ -28,8 +62,9 @@ indexapp.controller("IndexController", function($scope, $http) {
 				$scope.submitClass = 'btn';
 			});
 		} else {
+			console.log($modal);
 			$scope.submitClass = 'btn';
-		}
+		}*/
 		
 	};
 
@@ -45,5 +80,15 @@ indexapp.controller("IndexController", function($scope, $http) {
 		}).error(function(){
 
 		});
+	};
+});
+
+angular.module('indexapp').controller('ConfirmAddController', function ($scope, $modalInstance, submitObject) {
+	$scope.submitObject = submitObject;
+	$scope.ok = function (){
+		$modalInstance.close(submitObject);
+	};
+	$scope.cancel = function(){
+		$modalInstance.dismiss("cancel");
 	};
 });
